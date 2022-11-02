dfba7115d8cb8309425a89fcda4c33356bab4ce7
m4rk055 <marko@blindnet.io>
2022-11-02 10:21:54 +0100

    Fix token builder init

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ app.use(cors())
 const port = process.env.PORT
 const appId = process.env.APP_ID
 const key = process.env.APP_KEY
-const builder = TokenBuilder.init(appId, key)
 
 app.get('/token/user/:uid', (req, res) => {
-  builder.user(req.params.uid).then(
+  TokenBuilder.init(appId, key).user(req.params.uid).then(
     token => res.send({
       token
     })
