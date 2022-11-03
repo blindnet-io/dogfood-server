f15a791fa657a6eb4bf1b37fd5912432cb53ba01
m4rk055 <marko@blindnet.io>
2022-11-03 18:48:13 +0100

    Add OAuth token verification

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const jose = require('jose')
 const TokenBuilder = require('./TokenBuilder')
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 
-const port = process.env.PORT
-const appId = process.env.APP_ID
-const key = process.env.APP_KEY
+function env(name) {
+  const throwErr = () => { throw Error(`Environment variable ${name} not set`) }
+  return process.env[name] || throwErr()
+}
+
+const port = env('PORT')
+const appId = env('APP_ID')
+const key = env('APP_KEY')
+
+async function verify(jwt) {
+  const JWKS = jose.createRemoteJWKSet(new URL(env('AUTH0_PK_URL')))
+
+  const { payload } =
+    await jose.jwtVerify(jwt, JWKS, {
+      issuer: env('AUTH0_DOMAIN'),
+      audience: env('OAUTH_AUDIENCE'),
+    })
+
+  return payload
+}
 
 app.get('/token/user/:uid', (req, res) => {
   TokenBuilder.init(appId, key).user(req.params.uid).then(
@@ -18,6 +38,15 @@ app.get('/token/user/:uid', (req, res) => {
   )
 })
 
+app.get('/token/user', (req, res) => {
+  verify(req.body.access_token)
+    .then(payload =>
+      TokenBuilder.init(appId, key).user(payload['https://blindnet.io/email'])
+    )
+    .then(token => res.send({ token }))
+    .catch(_ => res.status(401).send())
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App started on port ${port}`)
 })
